6365cd770fa8acc232b436ffaf720f289a14265a
Kate Pronina <kate-pronina-97@yandex.ru>
2019-08-26 12:58:02 +0300

    Добавила проверку ввода, поиск по классу и debounce #6

--- src/components/kit/form/Form.js
+++ src/components/kit/form/Form.js
@@ -1,28 +1,59 @@
 class Form {
   constructor(formElement) {
     this.$formElement = formElement;
-    this.$inputs = formElement.find('.form__input');
+    this.$emailInput = formElement.find('.js-form__input-email');
+    this.$nameInput = formElement.find('.js-form__input-name');
+    this.$emailWarning = formElement.find('.js-form__warning-email');
+    this.$nameWarning = formElement.find('.js-form__warning-name');
+    this.emailRegExp = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+    this.nameRegExp = /[^0-9_.+]/;
     this.init();
   }
 
   init() {
-    this.$inputs.each((index, item) => {
-      $(item).on('input', this.onInput);
-    });
+    this.$emailInput.on('input', this.debounce(this.onEmailInput, 1000));
+    this.$nameInput.on('input', this.debounce(this.onNameInput, 1000));
   }
 
-  onInput = (event) => {
-    const { target } = event;
-    if (event.target.value) {
-      target.nextElementSibling.style.display = 'block';
-      target.nextElementSibling.textContent = 'Thanks!';
-      target.nextElementSibling.classList.remove('form__warning_error');
+  onEmailInput = (event) => {
+    if (this.checkInputValue(event.target.value, this.emailRegExp)) {
+      this.$emailWarning.css('display', 'block');
+      this.$emailWarning.text('Thanks!');
+      this.$emailWarning.removeClass('form__warning_error');
     } else {
-      target.nextElementSibling.style.display = 'block';
-      target.nextElementSibling.textContent = 'Error';
-      target.nextElementSibling.classList.add('form__warning_error');
+      this.$emailWarning.css('display', 'block');
+      this.$emailWarning.text('Error');
+      this.$emailWarning.addClass('form__warning_error');
     }
   }
+
+  onNameInput = (event) => {
+    if (this.checkInputValue(event.target.value, this.nameRegExp)) {
+      this.$nameWarning.css('display', 'block');
+      this.$nameWarning.text('Thanks!');
+      this.$nameWarning.removeClass('form__warning_error');
+    } else {
+      this.$nameWarning.css('display', 'block');
+      this.$nameWarning.text('Error');
+      this.$nameWarning.addClass('form__warning_error');
+    }
+  }
+
+  checkInputValue = (value, regex) => regex.test(value);
+
+  debounce = function debounce(func, wait, immediate) {
+    let timeout;
+    return function waitingCheck(...args) {
+      const later = () => {
+        timeout = null;
+        if (!immediate) func.apply(this, args);
+      };
+      const callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(this, args);
+    };
+  };
 }
 
 export default Form;
