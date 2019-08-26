997c2ebc38f8e384024fd799a3087e672dd02bb2
Kate Pronina <kate-pronina-97@yandex.ru>
2019-08-26 20:36:18 +0300

    Переписала циклы for #2

--- src/components/kit/button/button-init.js
+++ src/components/kit/button/button-init.js
@@ -3,7 +3,7 @@ import Button from './Button';
 $(() => {
   const buttons = document.querySelectorAll('.main-btn');
 
-  for (let i = 0; i < buttons.length; i += 1) {
-    new Button(buttons[i]);
-  }
+  buttons.forEach((button) => {
+    new Button(button);
+  });
 });
